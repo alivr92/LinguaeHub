28b189025bad5f457a4bf0cc5d71fb070912e31c
Lucas Vpr <Lucasvpr.dev@gmail.com>
2025-01-10 15:03:29 +0330

    "Commit by Pycharm"

--- tutors/models.py
+++ tutors/models.py
@@ -6,13 +6,23 @@ from ckeditor.fields import RichTextField
 
 class Tutor(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)  # Link to Django's User model
-    profile_picture = models.ImageField(upload_to='tutor_profiles/')
+    tutor_image = models.ImageField(upload_to='media/tutor_profiles/')
     bio = models.TextField()
     availability = models.CharField(max_length=255, blank=True)
-    languages = models.CharField(max_length=255, blank=True)  # Store languages as a comma-separated string
+    # rating star
     rating = models.FloatField(default=0.0)
     reviews_count = models.IntegerField(default=0)
     # Add other relevant fields like experience, teaching style, etc.
+
+    # location: city, state, country
+
+    # skills: Teaching Math, Teaching Piano, Teaching Languages: Arabic, Spanish, ...
+
+    # Languages: languages which the tutors can speak
+    languages = models.CharField(max_length=255, blank=True)  # Store languages as a comma-separated string
+
+
+
 
     def __str__(self):
         return f"{self.user.first_name} {self.user.last_name}"
